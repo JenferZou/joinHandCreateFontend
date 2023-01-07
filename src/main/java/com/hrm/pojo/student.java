1b5a053ced163b64ad6cd4d95fc1b1ecda6e2f3f
Ada <Ada@qq.com>
2023-01-07 17:20:24 +0800

    first

--- src/main/java/com/hrm/pojo/student.java
+++ src/main/java/com/hrm/pojo/student.java
@@ -3,5 +3,6 @@ package com.hrm.pojo;
 public class student {
     public static void main(String[] args){
         System.out.println("hellow");
+        System.out.println("helloworld..........");
     }
 }
