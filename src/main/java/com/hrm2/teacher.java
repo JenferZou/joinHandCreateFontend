8703621342419b4136a361bfc8af90add5c4525a
Jenfer <1634114623@qq.com>
2023-01-07 17:23:59 +0800

    by zjf2

---
+++ src/main/java/com/hrm2/teacher.java
@@ -0,0 +1,17 @@
+package com.hrm2;
+
+/**
+ * ClassName: teacher
+ * Description:
+ * date: 2023/1/7 17:22
+ *
+ * @author: Jenf
+ * @version:
+ * @since JDK 17
+ */
+public class teacher {
+    public static void main(String[] args) {
+        System.out.println("hellow teacher");
+    }
+
+}
