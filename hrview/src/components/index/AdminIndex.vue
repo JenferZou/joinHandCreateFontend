35992d8e83b8e1e271f917f8fcb6891b58daeeea
Ada <Ada@qq.com>
2024-01-16 10:28:56 +0800

    头像

--- hrview/src/components/index/AdminIndex.vue
+++ hrview/src/components/index/AdminIndex.vue
@@ -1,46 +1,50 @@
 <template>
-  <div class="layout" style="min-width: 1300px">
-    <div class="layout-header">
-      <i-row>
-        <i-col span="23">
-      <h1 style="margin-left: 10px">后台管理</h1>
-        </i-col>
-        <i-col span="1">
-          <div style="padding-top: 10px;">
-            <MyAvatar/>
-          </div>
-        </i-col>
-      </i-row>
-    </div>
-    <i-row type="flex">
-      <i-col span="4" class="layout-menu-left">
-        <i-menu @on-select="handleSelect" :active-name="sidebarItem"  width="auto">
-          <i-submenu name="2">
-            <template slot="title">
-              <i-icon type="ios-keypad"></i-icon>
-              信息管理
-            </template>
-            <i-menu-item name="index">学生信息</i-menu-item>
-            <i-menu-item name="project">项目管理</i-menu-item>
-              <i-menu-item name="teacher">老师信息</i-menu-item>
-              <i-menu-item name="analysis">数据分析</i-menu-item>
-          </i-submenu>
-          <i-submenu name="3">
-            <template slot="title">
-              <i-icon type="ios-person-outline"></i-icon>
-              系统管理
-            </template>
-            <i-menu-item name="userManagement">用户管理</i-menu-item>
-          </i-submenu>
-        </i-menu>
-      </i-col>
-      <i-col span="20">
-        <div class="layout-breadcrumb">
-          <i-breadcrumb>
-            <i-breadcrumb-item href="#" v-show="item.firstTitle!==''">{{ item.firstTitle}}</i-breadcrumb-item>
-            <i-breadcrumb-item href="#" v-show="item.title!==''">{{ item.title }}</i-breadcrumb-item>
-          </i-breadcrumb>
+    <div class="layout" style="min-width: 1300px">
+        <div class="layout-header">
+            <i-row>
+                <i-col span="23">
+                    <h5 style="color: #fff;margin-top:15px;margin-left:50px;font-weight: bolder;font-size: 20px">
+                        携手创-管理员端
+                    </h5>
+                </i-col>
+                <i-col span="1">
+                    <div style="padding-top: 10px;">
+
+                        <MyAvatar/>
+                    </div>
+                </i-col>
+            </i-row>
         </div>
+        <i-row type="flex">
+            <i-col span="4" class="layout-menu-left">
+                <i-menu @on-select="handleSelect" :active-name="sidebarItem" width="auto">
+                    <i-submenu name="2">
+                        <template slot="title">
+                            <i-icon type="ios-keypad"></i-icon>
+                            信息管理
+                        </template>
+                        <i-menu-item name="index">学生信息</i-menu-item>
+                        <i-menu-item name="project">项目管理</i-menu-item>
+                        <i-menu-item name="teacher">老师信息</i-menu-item>
+                        <i-menu-item name="analysis">数据分析</i-menu-item>
+                    </i-submenu>
+                    <i-submenu name="3">
+                        <template slot="title">
+                            <i-icon type="ios-person-outline"></i-icon>
+                            系统管理
+                        </template>
+                        <i-menu-item name="userManagement">用户管理</i-menu-item>
+                    </i-submenu>
+                </i-menu>
+            </i-col>
+            <i-col span="20">
+                <div class="layout-breadcrumb">
+                    <i-breadcrumb>
+                        <i-breadcrumb-item href="#" v-show="item.firstTitle!==''">{{ item.firstTitle }}
+                        </i-breadcrumb-item>
+                        <i-breadcrumb-item href="#" v-show="item.title!==''">{{ item.title }}</i-breadcrumb-item>
+                    </i-breadcrumb>
+                </div>
 
         <div class="layout-content" style="min-height: 550px">
           <router-view></router-view>
@@ -56,53 +60,53 @@
 <script>
 import MyAvatar from "@/components/views/admin/MyAvatar";
 export default {
-  name: "AdminIndex",
-  components: {MyAvatar},
-  data() {
-    return{
-      item:{
-        firstTitle:'',
-        title:'',
-        url:''
-      },
-      sidebarItem:'',
-    }
-  },
-  mounted() {
-    this.sidebarItem = this.$route.name;
-  },
-  methods: {
-    change(){
-      this.item.firstTitle=''
-      this.item.title = ''
+    name: "AdminIndex",
+    components: {MyAvatar},
+    data() {
+        return {
+            menuActive: false,
+            item: {
+                firstTitle: '',
+                title: '',
+                url: ''
+            },
+            sidebarItem: '',
+        }
     },
-    handleSelect(path) {
-      if(path==='index') {
-        this.item.firstTitle='信息管理'
-        this.item.title = '学生信息'
-      }
-      else if(path==='teacher') {
-        this.item.firstTitle='信息管理'
-        this.item.title = '老师信息'
-      }
-      else if(path==='project') {
-        this.item.firstTitle='信息管理'
-        this.item.title = '项目管理'
-      }
-      else if(path==='analysis') {
-          this.item.firstTitle='信息管理'
-          this.item.title = '数据分析'
-      }
-      else if(path==='userManagement') {
-        this.item.firstTitle=' 系统管理'
-        this.item.title = '用户管理'
-      }
-      this.sidebarItem = path;
-      this.$router.push({
-        name:path
-      })
+    mounted() {
+        this.sidebarItem = this.$route.name;
+    },
+    methods: {
+        toggleMenu() {
+            this.menuActive = !this.menuActive;
+        },
+        change() {
+            this.item.firstTitle = ''
+            this.item.title = ''
+        },
+        handleSelect(path) {
+            if (path === 'index') {
+                this.item.firstTitle = '信息管理'
+                this.item.title = '学生信息'
+            } else if (path === 'teacher') {
+                this.item.firstTitle = '信息管理'
+                this.item.title = '老师信息'
+            } else if (path === 'project') {
+                this.item.firstTitle = '信息管理'
+                this.item.title = '项目管理'
+            } else if (path === 'analysis') {
+                this.item.firstTitle = '信息管理'
+                this.item.title = '数据分析'
+            } else if (path === 'userManagement') {
+                this.item.firstTitle = ' 系统管理'
+                this.item.title = '用户管理'
+            }
+            this.sidebarItem = path;
+            this.$router.push({
+                name: path
+            })
+        }
     }
-  }
 }
 </script>
 
@@ -151,4 +155,5 @@ export default {
   text-align: center;
   background-color: black;
 }
+
 </style>
--- hrview/src/components/views/admin/MyAvatar.vue
+++ hrview/src/components/views/admin/MyAvatar.vue
@@ -1,6 +1,7 @@
 <template>
   <el-dropdown  size="small" @command="handleCommand">
-    <el-avatar  icon="el-icon-user-solid"></el-avatar>
+    <el-avatar
+      src="https://ts2.cn.mm.bing.net/th?id=OIP-C.0ym0bg0dFWwJCA1S01_PGQHaNK&w=187&h=333&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"></el-avatar>
     <el-dropdown-menu slot="dropdown">
       <el-dropdown-item command="logout">退出登录</el-dropdown-item>
     </el-dropdown-menu>
@@ -25,6 +26,11 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style >
+.el-avatar img{
+    display: block;
+    height: 100%;
+    vertical-align: middle;
+    width: 40px;
+}
 </style>
